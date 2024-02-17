aeb2290102632d3a3d160d4d6e190181c241222b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-17 18:35:09 +0100

    [add]:async function and take n times random words

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const output = document.getElementById('random-words');
+
+// N random words
+const repeatWord = 8;
+let arrayWords = [];
+let generatedPhrase;
+
+// Async function wait all data 
+async function waitData() {
+  try {
+    for (let i = 0; i < repeatWord; i++) {
+      const response = await axios.get('https://flynn.boolean.careers/exercises/api/random/word');
+      arrayWords.push(response.data.response);
+    }
+    generatedPhrase = arrayWords.join(" "); 
+    output.innerHTML = `<div><p>${generatedPhrase}</p></div>`;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+waitData();
